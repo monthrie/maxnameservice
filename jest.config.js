1a82dfc014874c02ce0221fc3cac011e5fbe0d0b
monthrie <wmontgomery@gmail.com>
2023-07-14 23:10:15 +0100

    ffor testing with jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testMatch: [
+      '**/__tests__/**/*.js?(x)',
+      '**/?(*.)+(spec|test).js?(x)'
+    ],
+    testPathIgnorePatterns: [
+      '/node_modules/'
+    ],
+    verbose: true
+  };
